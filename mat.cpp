2b776b3d97a0387d31ae863238c891b361fd2713
Gal25 <92460450+Gal25@users.noreply.github.com>
2022-03-09 09:37:31 +0200

    Add files via upload

---
+++ mat.cpp
@@ -0,0 +1,10 @@
+#include "mat.hpp"
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+namespace ariel{
+    string mat(int a, int b, char x, char y){
+        return " ";
+    }
+}
---
+++ mat.hpp
@@ -0,0 +1,9 @@
+
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+namespace ariel{
+    string mat(int a, int b, char x, char y); 
+}
